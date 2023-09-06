cf4b85067f3b59a4ae6ae844d88f9e5c7ce11871
maryamelyaagoubi <123840457+maryamelyaagoubi@users.noreply.github.com>
2023-09-06 22:21:00 +0200

    Update ask_question_to_pdf.py
    version plus generale

--- hackathon/code/utils/src/ask_question_to_pdf.py
+++ hackathon/code/utils/src/ask_question_to_pdf.py
@@ -100,9 +100,12 @@ def gpt3_completion(question, prompt_eng):
 def ask_question_to_pdf(question):
     return gpt3_completion(question, document)
 
+def ask_question_to_user_u(doc):
+    return gpt3_completion("Poses moi une question à propos du texte que j'ai fourni", doc)
 
 def ask_question_to_user():
-    return ask_question_to_pdf("Poses moi une question à propos du texte que j'ai fourni")
+    return ask_question_to_user_u(document)
+
 
 def evaluate_answer(question, user_answer):
         reply= openai.ChatCompletion.create(
